69bc08e4099e2439a3f3da5919390b5db36c85e6
ngthihaiha <nguyenthihaiha8124@gmail.com>
2024-11-23 14:35:15 +0700

    giao diện login admin

--- resources/views/admin/users/footer.blade.php
+++ resources/views/admin/users/footer.blade.php
@@ -1,6 +1,6 @@
 <!-- jQuery -->
-<script src="template/admin/plugins/jquery/jquery.min.js"></script>
+<script src="{{ asset('template/admin/plugins/jquery/jquery.min.js') }}"></script>
 <!-- Bootstrap 4 -->
-<script src="template/admin/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="{{ asset('template/admin/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
 <!-- AdminLTE App -->
-<script src="template/admin/dist/js/adminlte.min.js"></script>
+<script src="{{ asset('template/admin/dist/js/adminlte.min.js') }}"></script>
